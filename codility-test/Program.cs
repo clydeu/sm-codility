de0851167482368a8a85e8ff83ecb57b49ae2a31
Clyde Uyenghua <clyde.uyenghua@zepben.com>
2021-02-23 20:30:26 +1100

    initial.

---
+++ codility-test/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+
+namespace codility_test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+
+    public static class Task1
+    {
+        public static int Function(int[] A)
+        {
+            if (A.Length == 0)
+                return 0;
+
+            var yMax = A.Max();
+
+            if (A.Length <= 2)
+                return yMax;
+
+            // initialize coordinates with 0
+            var coordinates = new int[A.Length, yMax];
+
+            for (var x = 0; x < A.Length; x++)
+            {
+                for (var y = 0; y < A[x]; y++)
+                {
+                    coordinates[x, y] = 1;
+                }
+            }
+
+            bool brushToggle = false;
+            int brushCount = 0;
+            for (var y = 0; y < yMax; y++)
+            {
+                for (var x = 0; x < A.Length; x++)
+                {
+                    if (coordinates[x, y] == 1)
+                        brushToggle = true;
+                    else if (brushToggle)
+                    {
+                        brushCount++;
+                        brushToggle = false;
+                    }
+
+                    if (brushCount > 1000000000)
+                        return -1;
+                }
+
+                if (brushToggle)
+                {
+                    brushCount++;
+                    brushToggle = false;
+                }
+            }
+
+            return brushCount;
+        }
+    }
+
+    public static class Task2
+    {
+        public static int Function(int[] A)
+        {
+            int N = A.Length;
+            int result = 0;
+            for (int i = 0; i < N; i++)
+                for (int j = N - 1; j > i; j--)
+                    if (A[i] != A[j])
+                        result = Math.Max(result, j - i);
+
+            return 0;
+        }
+    }
+
+    public static class Task3
+    {
+        public static int Function(int[] A, int K)
+        {
+            int n = A.Length;
+            int best = 0;
+            int count = 0;
+            for (int i = 0; i < n - K - 1; i++)
+            {
+                if (A[i] == A[i + 1])
+                    count = count + 1;
+                else
+                    count = 0;
+                if (count > best)
+                    best = count;
+            }
+            int result = best + 1 + K;
+
+            return result;
+        }
+    }
+}
---
+++ test/UnitTest1.cs
@@ -0,0 +1,56 @@
+using codility_test;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace test
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(new int[] { 5, 8 }, 8)]
+        [InlineData(new int[] { 1, 3, 2, 1, 2, 1, 5, 3, 3, 4, 2 }, 9)]
+        [InlineData(new int[] { 1, 1, 1, 1 }, 1)]
+        [InlineData(new int[] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 }, 6)]
+        [InlineData(new int[] { 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3 }, 11)]
+        [InlineData(new int[] { 0, 0 }, 0)]
+        [InlineData(new int[] { }, 0)]
+        public void Test1_test(int[] A, int result)
+        {
+            Task1.Function(A).Should().Be(result);
+        }
+
+        public static IEnumerable<object[]> Test2TestData()
+        {
+            yield return new object[] { Enumerable.Repeat(1, 1000000).ToArray(), 0 };
+
+            var a = new int[7501];
+            for (var i = 0; i <= 7500; i++)
+            {
+                a[i] = i;
+            }
+
+            yield return new object[] { a, 7500 };
+        }
+
+        [Theory]
+        [InlineData(new int[] { 4, 6, 2, 2, 6, 6, 4 }, 5)]
+        [MemberData(nameof(Test2TestData))]
+        public void Test2_test(int[] A, int result)
+        {
+            var array = Enumerable.Repeat(1, 1000000).ToArray();
+
+            Task2.Function(A).Should().Be(result);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4 }, 7)]
+        [InlineData(new int[] { 1, 1, 3, 3, 3, 4, 5, 5, 5, 5 }, 5)]
+        [InlineData(new int[] { 1, 1, 1, 1, 1, 3, 3, 3, 4, 5, 5, 5, 5 }, 7)]
+        public void Test3_test(int[] A, int result)
+        {
+            Task3.Function(A, 2).Should().Be(result);
+        }
+    }
+}
